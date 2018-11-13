d588b3154132d83e72a43a266e72561b489b169b
Eduardo Ribeiro <eduardo.soares@gira.com.br>
2018-11-13 15:21:07 -0200

    Buscando usuários e exibindo na Tela - OBS: icones provisórios

--- desafio/src/pages/Index.vue
+++ desafio/src/pages/Index.vue
@@ -1,5 +1,35 @@
 <template>
   <q-page class="flex flex-center">
+    <table>
+      <tbody>
+        <tr v-for="user in users" :key="user.id.value">
+          <td>
+            <img :src="user.picture.thumbnail" alt="">
+          </td>
+          <td>
+            {{ user.name.first }}
+          </td>
+          <td>
+            {{ user.email }}
+          </td>
+          <td>
+            {{ user.cell }}
+          </td>
+          <td>
+            {{ user.location.city }} - {{ user.location.state }}
+          </td>
+          <td>
+            <q-icon name="remove_circle"/>
+          </td>
+          <td>
+            <q-icon name="reply"/>
+          </td>
+          <td>
+            <q-icon name="save"/>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </q-page>
 </template>
 
@@ -8,6 +38,30 @@
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'Index',
+
+  data () {
+    return {
+      API_URL: 'https://randomuser.me/api/',
+      users: {}
+    }
+  },
+
+  created () {
+    this.getUsers()
+  },
+
+  methods: {
+    getUsers () {
+      const URL = `${this.API_URL}?results=100`
+
+      this.$axios
+        .get(URL)
+        .then(users => {
+          this.users = users.data.results
+        })
+        .catch(error => console.error(error))
+    }
+  }
 }
 </script>
